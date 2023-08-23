92708bcb516d61de4813244f6efdc0fa1bff674f
eunseongzzangzzangman <8ohowb@naver.com>
2023-08-23 16:38:57 +0900

    크롤링하기

---
+++ 230823/News_MySQL_Auto_Insert copy.py
@@ -0,0 +1,143 @@
+# 전체 코드 복사하기
+
+import time
+import bs4
+import urllib.request
+import ssl
+import pymysql
+from tkinter import *
+from tkinter import messagebox
+import re
+# 주의사항, 문자열 안에 특수문자 부분 체크 잘하기.
+# 현재 네이트 기사는 특정 시간 예 1분마다 새로운 기사가 나옴.
+# 그래서, 현재 페이지에 1페이지에서 20개 기사만 디비에 저장하기로 함.
+# 특정 시간 time.sleep(300), 5분정도 인터벌을 두고 저장하기로 함.
+# 예스24는 한페이지의 상품이 고정,
+# 뉴스는 실시간으로 계속 생성됨.
+
+# 아래 코드는 웹 상에 접근 시, 인증된 여부를 확인할 때 필요한 부분
+# ssl 설정을 none 했음
+# 맥북 테스, 아래 오류가 발생해서 임시 방편으로 처리한 코드
+ssl_context = ssl.SSLContext()
+ssl_context.verify_mode = ssl.CERT_NONE
+
+# 전역변수. 기사에서 가지고 올 정보만 선택한 변수
+imgLinkUrl, subject, link, press, pDate, pTime = "", "", "", "", "", ""
+# null = None
+# 함수 선언 부분
+
+
+# regex 이용해서 re.sub(매개변수1, 매개변수2, 매개변수3)
+# 매개변수1 - 특정 조건
+# 매개변수2 - 대체할 문자
+# 매개변수3 - regex를 적용할 문장
+def clean_text(inputString):
+    text_rmv = re.sub(
+        '[-=+,#/\?:^.@*\"※~ㆍ!』‘\(\)\[\]`\'…》\”\“\’·]', ' ', inputString)
+    return text_rmv
+
+
+# 가져올 정보를 pymysql을 이용해서, 실제로 추가하는 부분
+def insertData(title, artist, album, img, first):
+    # con : 파이썬 <-> mysql 연결하기 위한 인스턴스, 도구
+    # cur : 디비의 정보를 조작하기 위한 도구
+    con, cur = None, None
+    data1, data2, data3, data4 = "", "", "", ""
+    sql = ""
+    con = pymysql.connect(host='127.0.0.1', user='root', password='1234', database='bugs', charset='utf8')
+    cur = con.cursor()
+#    title` VARCHAR(200) NULL,
+#   `publisher` VARCHAR(45) NULL,
+#   `newsDate` VARCHAR(10) NULL,
+#   `newsTime` VARCHAR(6) NULL,
+#   `newsDetail` VARCHAR(200) NULL,
+#   `newsImgUrl` VARCHAR(200) NULL,
+    # data0 = data10
+    data1 = title
+    data2 = artist
+    data3 = album
+    data4 = img
+    # data1 = clean_text(subject)
+    # data2 = clean_text(press)
+    # data3 = clean_text(pDate)
+    # data4 = clean_text(pTime)
+    # data5 = clean_text(link)
+    # data6 = clean_text(imgLinkUrl)
+    
+    if first == True:
+        cur.execute("TRUNCATE TABLE bugstb")
+
+    try:
+        # print(null)
+        # print("데이터 실행전 확인")
+        # print(data1)
+        # print(data2)
+        # print(data3)
+        # print(data4)
+        # print(data5)
+        # print(data6)
+        query = "INSERT INTO bugstb (title, artist, album, bugsImg) VALUES (%s, %s, %s, %s)"
+        values = (data1, data2, data3, data4)
+        # sql = "INSERT INTO newsTable (title,publisher,newsDate,newsTime,newsDetail,newsImgUrl)  VALUES(data1 + "," + data2 + "," + data3 + "," +  data4 + "," + data5 + ", "+ data6)"
+        print("순서2 : sql 실행전 ")
+        print(f"sql : {query}")
+        cur.execute(query, values)
+
+
+        # cur.execute(query, values)
+    except:
+        print('================================')
+        print("순서3 : 예외 발생")
+        print('================================')
+        # messagebox.showerror('오류', '데이터 입력 오류가 발생함')
+    else:
+        print('================================')
+        print("순서3: 성공")
+        print('================================')
+        # messagebox.showinfo('성공', '데이터 입력 성공')
+    con.commit()
+    con.close()
+
+
+
+##
+page = 1
+count = 1
+firstYN = True
+nateUrl = "https://music.bugs.co.kr/chart"
+while True:
+        try:
+            # newsUrl = nateUrl + str(page)
+            newsUrl = nateUrl
+            # page += 1
+            htmlObject = urllib.request.urlopen(newsUrl, context=ssl_context)
+            webPage = htmlObject.read()
+            bsObject = bs4.BeautifulSoup(webPage, 'html.parser')
+            # break
+            tag_list = bsObject.findAll('tr')
+            # print(tag_list)
+            # print('###### 실시간 뉴스 속보 #######')
+            for tag in tag_list:
+                if count == 1:
+                    count += 1
+                    continue
+                if count != 12:
+                    title = tag.find('p', {'class' : 'title'}).text
+                    artist = tag.find('p', {'class' : 'artist'}).text
+                    album = tag.find('a', {'class' : 'album'}).text
+                    img = tag.find('img')
+                    insertData(title, artist, album, img['src'], firstYN)
+                    firstYN = False
+                    count += 1
+                else:
+                    break
+        except:
+            break
+        else:
+            time.sleep(30)
+            firstYN = True
+            count = 1
+            #if count == 12:
+            #    break
+    # 특정 시간마다 동작 여부
+        # time.sleep(60)
